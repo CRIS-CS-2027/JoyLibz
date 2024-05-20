983a76234f0ca1acef12ca8abfab82e0c0e69a3c
Prim <kulpreeya@cris.ac.th>
2024-05-20 09:31:31 +0200

    starting template

---
+++ templates/doctors-visit.py
@@ -0,0 +1,25 @@
+# Template - fill in blanks
+
+adj1 = 'funny'
+noun1 = 'tent'
+adj2 = 'yellow'
+adj3 = 'slow'
+noun2 = 'cake'
+body1 = 'leg'
+body2 = 'head'
+body3 = 'ears'
+adj4 = 'scary'
+noun3 = 'uncle'
+noun4 = 'car'
+noun5 = 'poster'
+adj5 = 'ugly'
+
+mad_text = f'''Every year, you should go visit the doctor. It is a very {adj1} visit.
+Usually, you have to skip going to {noun1} to go. Your doctor is usually
+a/an {adj2} man or woman who is wearing a/an {adj3} {noun2}.
+They will look at your {body1}, {body2}, and {body3}.
+Sometimes, they can be very {adj4} . Afterwards, they will give you a {noun3}
+ and a {noun4} and your mom or dad will take you to {noun5}
+ as a treat. All in all, the doctor's office isn't so {adj5}!'''
+
+print(mad_text)
