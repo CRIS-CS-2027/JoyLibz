85121e374b47b5cf96a1fadf7d139180676a15c9
Tina <tina.y@cris.ac.th>
2024-05-24 10:17:17 +0700

    completed word list function

--- main.py
+++ main.py
@@ -8,26 +8,10 @@ def get_word_list(filename):
         words_list = []
         reader = csv.DictReader(words, skipinitialspace=True)
         for row in reader:
-        #     print(row)
-        #     for words in row.items():
-        #         for letter in words:
-        #             if letter == 0:
-        #                 letter == "False"
-        #             elif letter == 1:
-        #                 letter == "True"
-        #             print(letter)
+            row['plural'] = bool(int(row['plural']))
             words_list += [row]
         return words_list
 
 my_list = get_word_list("words-lists/nouns.csv")
 
-for list in my_list:
-    for words in list.items():
-        for plural in words:
-            if plural == "0":
-                print("False")   #idk how to put it into the list
-            elif plural == "1":
-                print("True")
-            else:
-                continue
 print(my_list)
