d7fdf48111aa0de36540727e835cce9257c75c8f
Teacher Isaac <teacher@eduk8.us>
2024-05-25 11:31:02 +0200

    randwords() now using the words-lists/{nouns,verbs,adjectives}.csv files

--- data/word_lists.py
+++ data/word_lists.py
@@ -1,17 +1,41 @@
 import csv
+from pprint import pprint
 
-def get_word_list(filename):
+def dict_val(row):
+    '''returns given csv.DictReader row as a dictionary'''
+    try:
+        row['plural'] = bool(int(row['plural']))
+    except:
+        row['plural'] = None
+    return row
+
+def tuple_val(row):
+    '''returns given csv.DictReader row as a tuple'''
+    row = dict_val(row)
+    return (row['word'], row['topic'], row['plural'])
+
+def get_word_list(filename, item_type='dict'):
     """
-    A function that opens a CSV file and returns a list of dictionaries of data
+    opens CSV file and returns list of <item_type> data
+    where <item_type> in ('dict', 'tuple')
     """
+    val_func = dict_val if item_type=='dict' else tuple_val
+
     with open(filename, encoding ='utf-8') as words:
         words_list = []
         reader = csv.DictReader(words, skipinitialspace=True)
         for row in reader:
-            row['plural'] = bool(int(row['plural']))
-            words_list += [row]
+            words_list += [val_func(row)]
         return words_list
 
 if __name__ == '__main__':
-    my_list = get_word_list("../words-lists/nouns.csv")
-    print(my_list)
+    import sys
+    if len(sys.argv) > 1:
+        fname = sys.argv[1]
+    else:
+        fname = "../words-lists/nouns.csv"
+    dict_list = get_word_list(fname)
+    pprint(dict_list)
+
+    tuple_list = get_word_list(fname, 'tuple')
+    pprint(tuple_list)
