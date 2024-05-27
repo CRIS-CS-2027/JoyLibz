e2e7c962080d1a2e863a48d16f3bad8fefa17013
Kulpreeya <kulpreeya@cris.ac.th>
2024-05-27 09:32:46 +0200

    added new templates

---
+++ templates/she_walks_in_beauty.py
@@ -0,0 +1,18 @@
+import os
+import sys
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+from data.random_words import randword
+
+# modified public domain poem 
+# She Walks in Beauty, by Lord Byron
+# https://hitrecord.org/records/3565436
+
+mad_text = f'''Her {randword('noun', 'body_parts')} in beauty, like the {randword('noun', 'place')} 
+Of {randword('adj', 'action')} climes and {randword('adj', '')} skies
+And all that’s {randword('adj', 'action')} of {randword('adj', '')} and {randword('adj', 'action')}
+Meet in her aspect and her {randword('noun', 'body_parts')} Thus mellowed to that tender {randword('noun', '')}
+Which heaven to {randword('adj', 'action')} {randword('noun', '')} denies.'''
+
+print(mad_text)
